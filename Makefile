621dcd05dec77497b606c1234d130550a4524a5c
Oleksandr (Sasha) Huziy <huziy@beluga.cluster10.local>
2014-03-20 12:52:56 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+FC = s.f90
+CC = s.cc
+
+
+
+SOURCES_F = $(wildcard *.f90)
+OBJ = $(patsubst %.f90, %.o, $(notdir $(SOURCES_F))) 
+
+INC = $(foreach d, $(EC_INCLUDE_PATH), -I$d) -I$(shell nc-config --includedir)
+
+EXEC_NAME = test.exe
+
+#rmnlib_folder = $(dir $(shell s.locate --lib rmnshared_013))
+
+LIBS = $(shell nc-config --libs) -lnetcdff 
+
+all : $(OBJ) 
+	@echo $(OBJ)
+	$(FC) $(OBJ) -o $(EXEC_NAME) $(LIBS) 
+
+
+$(OBJ) : $(SOURCES_F) 
+	echo $<
+	echo $@
+	$(FC) -c $< -o $@  
+	#gfortran -fPIC -c -g $< -o $@
+
+
+
+clean:
+	rm -f $(OBJ) $(EXEC_NAME)
---
+++ main.f90
@@ -0,0 +1,81 @@
+
+        program test
+         use netcdf
+         implicit none 
+                
+
+        
+         ! This is the name of the data file we will read. 
+         character (len = *), parameter :: FILE_NAME = "bathy_meter.nc"
+
+        ! We are reading 2D data, a 6 x 12 grid. 
+        integer :: nx, ny, ndims
+        real, allocatable :: data_in(:, :)
+        
+
+        ! This will be the netCDF ID for the file and data variable.
+        integer :: ncid, varid, x_dim_id, y_dim_id
+
+        ! Loop indexes, and error handling.
+        integer :: x, y, dim_ids(nf90_max_var_dims), i
+        integer :: include_parents
+
+        character(len=NF90_MAX_NAME) :: xname, yname        
+
+
+        ! Open the file. NF90_NOWRITE tells netCDF we want read-only access to
+        ! the file.
+        call check( nf90_open(FILE_NAME, NF90_NOWRITE, ncid) )
+
+        ! Get the varid of the data variable, based on its name.
+        call check( nf90_inq_varid(ncid, "Bathymetry", varid) )
+
+        !Get var dimensions
+        dim_ids = -1
+        include_parents = 0
+        call check(nf90_inq_dimids(ncid, ndims, dim_ids, include_parents))
+        do i = 1, ndims
+                print*, dim_ids(i)
+        enddo
+        x_dim_id = dim_ids(2)
+        y_dim_id = dim_ids(1)
+
+        call check(nf90_inquire_dimension(ncid, y_dim_id, xname, nx))
+        call check(nf90_inquire_dimension(ncid, y_dim_id, yname, ny))
+
+        print*, xname, ", ", yname
+        print*, nx, ", ", ny
+
+        allocate(data_in(ny, nx))                
+
+        ! Read the data.
+        call check( nf90_get_var(ncid, varid, data_in) )
+
+
+
+        ! Check the data.
+        do x = 1, nx
+        do y = 1, ny
+              print *, "data_in(", y, ", ", x, ") = ", data_in(y, x)
+        end do
+        end do
+
+        ! Close the file, freeing all resources.
+        call check( nf90_close(ncid) )
+
+        print *,"*** SUCCESS reading example file ", FILE_NAME, "! "
+
+
+
+         contains
+          subroutine check(status)
+                integer, intent ( in) :: status
+    
+                if(status /= nf90_noerr) then 
+                        print *, trim(nf90_strerror(status))
+                        stop "Stopped"
+                end if
+          end subroutine check  
+
+
+        end program test
